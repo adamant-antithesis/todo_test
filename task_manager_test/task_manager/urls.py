0a74472885609c1947c60fd280edd2e4e728add3
Yurii Koidan <y.koidan@andersenlab.com>
2024-03-05 21:54:51 +0200

    Added styles for task detail page

--- task_manager_test/task_manager/urls.py
+++ task_manager_test/task_manager/urls.py
@@ -5,7 +5,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('', LoginView.as_view(), name='login'),
-    path('tasks', views.task_list, name='task_list'),
+    path('tasks/', views.task_list, name='task_list'),
     path('task/<int:task_id>/', views.task_detail, name='task_detail'),
     path('create/', views.create_task, name='create_task'),
     path('registration/', views.signup_view, name='signup'),
